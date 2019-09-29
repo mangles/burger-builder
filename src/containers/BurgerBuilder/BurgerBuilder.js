f72a4460e687a72db81b3fcdf963a8a99868aac6
manglestw <mangles@thoughtworks.com>
2019-09-29 11:47:18 +0200

    Structure layout and start building Burger

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger'
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>
+                    <Burger/>
+                    <div>Build controls</div>
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder
