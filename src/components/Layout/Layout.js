022035bd57f83329531dfa596bc1c722368d37b0
manglestw <mangles@thoughtworks.com>
2019-10-01 20:13:05 +0200

    Add Toggle menu to show side drawer or not

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../hoc/Aux'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.css'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-const layout = (props) => (
-    <Aux>
-        <div>
-            <Toolbar/>
-            <SideDrawer/>
-        </div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
 
-export default layout
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <div>
+                    <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                    <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer} />
+                </div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
